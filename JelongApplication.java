af857bd908e5e439330d784bfbbfb38471f35916
jay <442050368@qq.com>
2018-04-03 18:00:46 +0800

    1234

---
+++ JelongApplication.java
@@ -0,0 +1,13 @@
+package com.example.jelong.jelong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JelongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JelongApplication.class, args);
+	}
+
+}
